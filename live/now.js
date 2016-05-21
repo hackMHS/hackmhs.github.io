2d1a61e06890ed897156557dc3f80614b642ce41
Shannon Peng <shannonpeng98@gmail.com>
2016-05-21 15:17:26 -0400

    Update announcement again

--- live/now.js
+++ live/now.js
@@ -1,6 +1,6 @@
 app.controller('NowController', function($scope) {
 
-	$scope.tweet = "Web Design workshop at 2pm in Room 108! Intro to Java in Room 107.";
+	$scope.tweet = "Software Design in 109; Javascript &amp; APIs in 112!";
 
 	// ??
 	
