cdb6d6b585c43de554a7b56548d9499433819223
Shannon Peng <shannonpeng@users.noreply.github.com>
2016-05-22 11:50:50 -0400

    Update now.js

--- live/now.js
+++ live/now.js
@@ -1,6 +1,6 @@
 app.controller('NowController', function($scope) {
 
-	$scope.tweet = "The list of projects being demoed is posted in the Facebook group!\n After you finish lunch, start gathering your things and heading up to the auditorium.\n No food is allowed in the auditorium.";
+	$scope.tweet = "Head to http://bit.ly/hackmhs-demo\n for the demo order! After you finish lunch, start gathering your things and heading up to the auditorium.\n No food is allowed in the auditorium.";
 
 	// ??
 	
